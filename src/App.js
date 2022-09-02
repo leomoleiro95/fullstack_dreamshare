9ac9155f5001e240b47db864b058b0b6f51a411a
Leonardo Moleiro <lmoleiro@google.com>
2022-09-02 15:36:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { Container} from "@mui/material";
 
-function App() {
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+
+const clientGID = process.env.GOOGLE_CLIENT_ID
+const theme = createTheme({
+
+});
+
+const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'))
+  //console.log("App.js User", user);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleOAuthProvider clientId={clientGID} >
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <Container maxwidth="xl">
+            <Navbar />
+            <Switch>
+              <Route path="/" exact component={() => <Redirect to="/posts" />} />
+              <Route path="/posts" exact component={Home} />
+              <Route path="/posts/search" exact component={Home} />
+              <Route path="/posts/:id" component={PostDetails} />
+              <Route path="/auth" exact component={() => !user ? ( <Auth  /> ) : ( <Redirect to="/posts" /> )}
+              />
+            </Switch>
+          </Container>
+        </ThemeProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   );
-}
+};
 
 export default App;
