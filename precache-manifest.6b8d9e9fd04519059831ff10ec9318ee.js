9e9f209e04ca8d648727b6fa8fef7f678b06f0dc
leomoleiro95 <leomoleiro@gmail.com>
2022-09-04 13:08:31 -0500

    Updates

---
+++ precache-manifest.6b8d9e9fd04519059831ff10ec9318ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02843b114f2c8b000817c8bddef9afe6",
+    "url": "/fullstack_dreamshare/index.html"
+  },
+  {
+    "revision": "41f00de2d66512269b84",
+    "url": "/fullstack_dreamshare/static/css/main.2f765595.chunk.css"
+  },
+  {
+    "revision": "b0e7321b00ddfb9a3a23",
+    "url": "/fullstack_dreamshare/static/js/2.d612c036.chunk.js"
+  },
+  {
+    "revision": "1a1803a22b03bb07b77d827b47f236d9",
+    "url": "/fullstack_dreamshare/static/js/2.d612c036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f00de2d66512269b84",
+    "url": "/fullstack_dreamshare/static/js/main.8322dfc2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/fullstack_dreamshare/static/js/main.8322dfc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b3158735ed2d58719d",
+    "url": "/fullstack_dreamshare/static/js/runtime-main.ba2dea84.js"
+  }
+]);
